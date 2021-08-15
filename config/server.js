3eee43f2f531c8c53a07bbd221668ed32f0fae0e
Edmundo Paulino <manyymoore@gmail.com>
2021-08-15 07:54:45 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ae27ce0ee0c093300761374e92536cd2'),
+    },
+  },
+});
