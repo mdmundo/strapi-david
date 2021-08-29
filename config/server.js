df0b8cf32a9dc60c2e8066ad0ba4c932ba51eeca
Edmundo Paulino <60408300+mdmundo@users.noreply.github.com>
2021-08-29 14:04:43 -0300

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: 'https://0l5ox8r4.anyfiddle.run',
+  url: env('URL', 'https://0l5ox8r4.anyfiddle.run'),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'ae27ce0ee0c093300761374e92536cd2'),
